3efbb97efda72ae4b7f871c61183e814da23b502
Philip <stuckinsnow@users.noreply.github.com>
2025-07-10 23:41:35 +0100

    fix: use short hash

--- lua/gitty/providers/github-compare/blame-utils.lua
+++ lua/gitty/providers/github-compare/blame-utils.lua
@@ -16,7 +16,8 @@ function M.copy_blame_commit_hash_for_current_line()
 
 	local line = vim.api.nvim_win_get_cursor(0)[1]
 	local relative_path = vim.fn.fnamemodify(file, ":~:.")
-	local cmd = string.format("git blame -L %d,%d --porcelain %s", line, line, vim.fn.shellescape(relative_path))
+	local cmd =
+		string.format("git blame -L %d,%d --porcelain --abbrev=7 %s", line, line, vim.fn.shellescape(relative_path))
 
 	local handle = io.popen(cmd)
 	local result = handle and handle:read("*a") or ""
@@ -32,8 +33,9 @@ function M.copy_blame_commit_hash_for_current_line()
 	-- Parse the blame output to get the commit hash
 	local hash = result:match("^(%w+)")
 	if hash then
-		vim.fn.setreg("+", hash)
-		vim.notify("Blame commit hash for line " .. line .. ": " .. hash, vim.log.levels.INFO)
+		local short_hash = hash:sub(1, 7)
+		vim.fn.setreg("+", short_hash)
+		vim.notify("Blame commit hash for line " .. line .. ": " .. short_hash, vim.log.levels.INFO)
 	else
 		vim.notify("Failed to extract commit hash from blame output", vim.log.levels.ERROR)
 	end
